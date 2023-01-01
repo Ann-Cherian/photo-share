b49582d4f50a104844e2ae118844218e5b926358
ann <annbyjus@gmail.com>
2023-01-01 23:58:46 +0530

    initialheading

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <head>
+        <title>Create Next App
+        </title>
+      </head>
+      <h1>This is the INSTAGRAM 2.0 BUILD</h1>
+    </div>
+  );
+}
