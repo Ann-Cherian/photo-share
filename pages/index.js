93ffc7d1385f40dff159241f410a4ebedd482739
ann <annbyjus@gmail.com>
2023-01-05 18:01:39 +0530

    Create middle of Header

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,15 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Instagram 2.0  </title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <h1>This is the INSTAGRAM 2.0 BUILD</h1>
+      
+
       {/* Header */}
-      <Header />
+      <Header/>
 
       {/* Feed */}
 
