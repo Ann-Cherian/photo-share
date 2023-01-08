870d254d28007dd74750af98469b7d92a489ebd7
ann <annbyjus@gmail.com>
2023-01-08 22:48:49 +0530

    Changed faker package and added styles to story image

--- components/Stories.js
+++ components/Stories.js
@@ -1,4 +1,4 @@
-import faker from "faker";
+import {faker} from "@faker-js/faker";
 import { useEffect, useState } from "react";
 import Story from "./Story"
 
@@ -7,14 +7,20 @@ function Stories() {
 
     useEffect(() => {
         const suggestions = [...Array(20)].map((_, i) => ({
-            ...faker.helpers.contextualCard(), id: i,
+            userId: faker.datatype.uuid(),
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            avatar: faker.image.avatar(),
+            password: faker.internet.password(),
+            birthdate: faker.date.birthdate(),
+            registeredAt: faker.date.past(),
         }));
 
         setSuggestions(suggestions);
     }, []);
 
     return (
-        <div>
+        <div className="flex space-x-2 p-6  bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll ">
             {suggestions.map(profile => (
                 <Story
                 key={profile.id}
